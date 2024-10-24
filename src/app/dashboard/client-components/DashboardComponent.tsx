1476bd076b6913f0a0fb8cfdcd7f405c1e4a9e14
Liv <110914989+livoszlak@users.noreply.github.com>
2024-10-24 14:40:31 +0200

    DashboardComponent can output context data

--- src/app/dashboard/client-components/DashboardComponent.tsx
+++ src/app/dashboard/client-components/DashboardComponent.tsx
@@ -1,19 +1,30 @@
 "use client";
 import Link from "next/link";
-import { useContext, useEffect } from "react";
-import { AuthContext } from "@/contexts/AuthContext";
+import { useContext, useEffect, useState } from "react";
+/* import { AuthContext } from "@/contexts/AuthContext"; */
 import { DashboardContext } from "@/contexts/DashboardContext";
 
 export default function DashboardComponent() {
-  const authContext = useContext(AuthContext);
+  /* const authContext = useContext(AuthContext); */
   const dashboardContext = useContext(DashboardContext);
+  const [greeting, setGreeting] = useState("");
+
+  useEffect(() => {
+    if (
+      dashboardContext &&
+      dashboardContext.contextOrgs &&
+      dashboardContext.contextOrgs.length > 0
+    ) {
+      setGreeting(dashboardContext.contextOrgs[0].greeting);
+    }
+  }, [dashboardContext]);
 
   return (
     <>
       {dashboardContext && (
         <div>
           <h1>dashboad context finnes</h1>
-          <h2>{dashboardContext.contextOrgs[0].greeting}</h2>
+          <h2>{greeting}</h2>
         </div>
       )}
       <Link href={"/dashboard/classics"}>Go to Classics</Link>
